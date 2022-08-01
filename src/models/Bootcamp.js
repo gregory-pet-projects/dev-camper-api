c40861d14adf2c7b074a549f08ce2697c8daf070
Gregory Petrov <grishap96123@gmail.com>
2022-08-01 20:50:15 +0300

    added slugify to precreate changes

--- src/models/Bootcamp.js
+++ src/models/Bootcamp.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const slugify = require('slugify')
 const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z\d@:%._+~#=]{1,256}\.[a-zA-Z\d()]{1,6}\b([-a-zA-Z\d()@:%_+.~#?&/=]*)/
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}])|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/
 
@@ -92,5 +92,11 @@ const BootcampSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+//Create bootcamp slug from the schema
+BootcampSchema.pre('save', function (next) {
+    this.slug = slugify(this.name, {lower: true})
+    next()
+})
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)

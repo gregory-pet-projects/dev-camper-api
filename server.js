04e86041b914c8329d40ea3e8d09a55a7f0ed21e
Gregory Petrov <grishap96123@gmail.com>
2022-08-01 15:47:35 +0300

    added colors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const bootcamps = require('./src/routes/bootcamps')
 const connectDB = require('./src/config/db')
+require('colors')
 
 dotenv.config({path: './src/config/config.env'})
 
@@ -18,10 +19,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 3300
-const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 //Handler unhandled promise rejections
 process.on('unhandledRejection', (err, promise)=>{
-    console.log(`Error: ${err.message}`)
+    console.log(`Error: ${err.message}`.red)
     server.close(()=>process.exit(1))
 })
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI)
-    console.log(`Mongo DB connected ${conn.connection.host}`)
+    console.log(`Mongo DB connected ${conn.connection.host}`.cyan.underline.bold)
 }
 
 module.exports = connectDB

22c4c8f63e916ee3dc7918474cee3d388c61497e
Gregory Petrov <grishap96123@gmail.com>
2022-08-02 22:43:48 +0300

    changed controller name

--- src/controllers/courses.js
+++ src/controllers/courses.js
@@ -6,7 +6,28 @@ const asyncHandler = require('../middleware/async');
 //@route GET /api/v1/courses
 //@route GET /api/v1/bootcamps/:bootcampID/courses
 //@access Public
-exports.getCourses = asyncHandler(async (req, res, nxt) => {
+exports.getListCourses = asyncHandler(async (req, res, nxt) => {
+  let query;
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = Course.find().populate({
+      path: 'bootcamp',
+      select: 'name description',
+    });
+  }
+  const courses = await query;
+  res.status(200).json({
+    success: true,
+    count: courses.length,
+    data: courses,
+  });
+});
+
+//@desc Get single course
+//@route GET /api/v1/courses/:id
+//@access Public
+exports.getCourse = asyncHandler(async (req, res, nxt) => {
   let query;
   if (req.params.bootcampId) {
     query = Course.find({ bootcamp: req.params.bootcampId });

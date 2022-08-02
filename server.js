7d069a49ece5b916c660a2afe4d5a67e4e3ec564
Gregory Petrov <grishap96123@gmail.com>
2022-08-02 21:56:12 +0300

    connected route

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
-const express = require('express')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const connectDB = require('./src/config/db')
-require('colors')
-const errorHandler = require('./src/middleware/error')
-dotenv.config({path: './src/config/config.env'})
-const bootcamps = require('./src/routes/bootcamps')
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./src/config/db');
+require('colors');
+const errorHandler = require('./src/middleware/error');
+dotenv.config({ path: './src/config/config.env' });
+const bootcamps = require('./src/routes/bootcamps');
+const courses = require('./src/routes/courses');
 
-const app = express()
+const app = express();
 
 //Body parser
-app.use(express.json())
+app.use(express.json());
 
 //DB
-connectDB()
-
-
+connectDB();
 
 //Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
 
-app.use('/api/v1/bootcamps', bootcamps)
+app.use('/api/v1/bootcamps', bootcamps);
+app.use('/api/v1/courses', courses);
 
-app.use(errorHandler)
-const PORT = process.env.PORT || 3300
-const server = app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.use(errorHandler);
+const PORT = process.env.PORT || 3300;
+const server = app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
 
 //Handler unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error: ${err.message}`.red)
-    server.close(() => process.exit(1))
-})
+  console.log(`Error: ${err.message}`.red);
+  server.close(() => process.exit(1));
+});

aa69582efcb1ec8544d266add1ade6829f5705c4
Gregory Petrov <grishap96123@gmail.com>
2022-08-02 21:33:09 +0300

    added data to seeder

--- seeder.js
+++ seeder.js
@@ -5,23 +5,27 @@ const dotenv = require('dotenv')
 
 //Load env vars
 dotenv.config(({path: './src/config/config.env'}))
+
 const Bootcamp = require('./src/models/Bootcamp')
+const Course = require('./src/models/Course')
+
 
 //Connect to db
 const connect = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log(`Mongo DB connected ${conn.connection.host}`.cyan.underline.bold)
 }
- connect()
+connect()
 //Read JSON files
-const bootcamps = JSON.parse(
-    readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
-)
+const bootcamps = JSON.parse(readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'))
+
+const courses = JSON.parse(readFileSync(`${__dirname}/_data/courses.json`, 'utf-8'))
 
 //Imports into db
 const importData = async () => {
     try {
         await Bootcamp.create(bootcamps)
+        await Course.create(courses)
         console.log('Data imported...'.green.inverse)
         process.exit()
     } catch (e) {
@@ -32,6 +36,7 @@ const importData = async () => {
 const deleteData = async () => {
     try {
         await Bootcamp.deleteMany()
+        await Course.deleteMany()
         console.log('Data destroyed...'.red.inverse)
         process.exit()
     } catch (e) {
@@ -43,4 +48,7 @@ if (process.argv[2] === '-i') {
     importData()
 } else if (process.argv[2] === '-d') {
     deleteData()
+} else if (process.argv[2] === '-u') {
+    deleteData()
+    importData()
 }

b1baa7740993c706d4644295f14faaad3d4fbf92
Gregory Petrov <grishap96123@gmail.com>
2022-08-02 22:22:11 +0300

    include courses resource router into bootcamps

--- src/routes/bootcamps.js
+++ src/routes/bootcamps.js
@@ -1,26 +1,29 @@
-const express = require('express')
+const express = require('express');
 const {
-    getListBootcamps,
-    getBootcamp,
-    createBootcamp,
-    updateBootcamp,
-    deleteBootcamp,
-    getBootcampsInRadius
-} = require('../controllers/bootcamps')
-const router = express.Router()
+  getListBootcamps,
+  getBootcamp,
+  createBootcamp,
+  updateBootcamp,
+  deleteBootcamp,
+  getBootcampsInRadius,
+} = require('../controllers/bootcamps');
 
-router.route('/radius/:postcode/:distance')
-    .get(getBootcampsInRadius)
+//Include other resource router
+const courseRouter = require('./courses');
 
-router.route('/')
-    .get(getListBootcamps)
-    .post(createBootcamp)
+const router = express.Router();
 
-router.route('/:id')
-    .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp)
+//Re-route into other resource routes
+router.use('/:bootcampId/courses', courseRouter);
 
+router.route('/radius/:postcode/:distance').get(getBootcampsInRadius);
 
+router.route('/').get(getListBootcamps).post(createBootcamp);
 
-module.exports = router
+router
+  .route('/:id')
+  .get(getBootcamp)
+  .put(updateBootcamp)
+  .delete(deleteBootcamp);
+
+module.exports = router;

54ce6a8206133fae1d14a90d423c868b09aee344
Gregory Petrov <grishap96123@gmail.com>
2022-08-03 15:13:00 +0300

    added bcrypt to password

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { emailRegex } = require('../utils/regex');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,6 +30,12 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now(),
   },
+});
+
+//Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('Users', UserSchema);

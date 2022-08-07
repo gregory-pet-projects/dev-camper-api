a1df99bf56b1cbd929c498fb0993dae141e8c157
Gregory Petrov <grishap96123@gmail.com>
2022-08-07 23:31:41 +0300

    added user routes (protected)

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,9 +6,19 @@ const {
   updateUser,
   deleteUser,
 } = require('../controllers/users');
+const User = require('../models/User');
+const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router();
 //Protect route middleware
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
+
+//Protect for all routes
+router.use(protect);
+router.use(authorize('admin'));
+
+router.route('/').get(advancedResults(User), getListUsers).post(createUser);
+
+router.route('/:id').get(getUser).put(updateUser).delete(deleteUser);
 
 module.exports = router;

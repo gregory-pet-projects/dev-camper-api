7e2a374cba9ed1bf4f7bdcfa4429fc01b5a854da
Gregory Petrov <grishap96123@gmail.com>
2022-08-07 22:33:27 +0300

    added update user details

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -139,8 +139,27 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
   // Set new password
   user.password = req.body.password;
   user.resetPasswordToken = undefined;
-  user.resetPasswordExpire = undefined;
+  user.resetPasswordExpired = undefined;
   await user.save();
 
   sendTokenResponse(user, 200, res);
+});
+
+//@desc Update user details
+//@route Put /api/v1/auth/updatedetails
+//@access Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+  const fieldsToUpdate = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+  const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
 });

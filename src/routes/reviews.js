64808f72e94a8580a7bc4f6f28d0f4cec3d492da
Gregory Petrov <grishap96123@gmail.com>
2022-08-08 00:20:34 +0300

    added reviews router

---
+++ src/routes/reviews.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const {} = require('../controllers/reviews');
+const Review = require('../models/Review');
+
+const router = express.Router();
+
+module.exports = router;

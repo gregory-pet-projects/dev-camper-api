ed8c20073a561884415ce1c4cce62f355fc185b9
Gregory Petrov <grishap96123@gmail.com>
2022-08-08 15:32:27 +0300

    adde controller for add review

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const { getListReviews, getReview } = require('../controllers/reviews');
+const {
+  getListReviews,
+  getReview,
+  addReview,
+} = require('../controllers/reviews');
 const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResults');
 
 const router = express.Router({ mergeParams: true });
+const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').get(
-  advancedResults(Review, {
-    path: 'bootcamp',
-    select: 'name description',
-  }),
-  getListReviews
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Review, {
+      path: 'bootcamp',
+      select: 'name description',
+    }),
+    getListReviews
+  )
+  .post(protect, authorize('user', 'admin'), addReview);
 
 router.route('/:id').get(getReview);
 
